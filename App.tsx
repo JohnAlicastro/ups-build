6ee96d636674e8a30f2b9285b48d96ddf5942af1
John Alicastro <93687275+JohnAlicastro@users.noreply.github.com>
2023-06-28 22:00:22 -0400

    Create README.md + fix Expo Go app connectivity issue

--- App.tsx
+++ App.tsx
@@ -1,27 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
 import { TailwindProvider } from 'tailwind-rn';
+import { NavigationContainer } from '@react-navigation/native';
 import utilities from './tailwind.json';
-import CustomerScreen from './screens/CustomerScreen';
+import RootNavigator from './navigator/RootNavigator';
 
 export default function App() {
   return (
     // @ts-ignore - TailwindProvider is missing a type definition
     <TailwindProvider utilities={utilities}>
       <NavigationContainer>
-        <CustomerScreen />
+        <RootNavigator />
       </NavigationContainer>
     </TailwindProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ README.md
@@ -0,0 +1,13 @@
+RUN THE FOLLOWING TWO COMMANDS TO SPIN UP THE APP
+
+```
+npx expo start
+```
+
+(This is to spin up the Expo server)
+
+```
+npm run dev:tailwind
+```
+
+(This is to spin up the tailwind-rn compiler)
