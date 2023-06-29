f216d06f9a8bb71bbed5caaa4d200bfec5c71cf0
John Alicastro <93687275+JohnAlicastro@users.noreply.github.com>
2023-06-29 19:48:08 -0400

    Hide header via TabNavigator.tsx

--- App.tsx
+++ App.tsx
@@ -11,5 +11,5 @@ export default function App() {
         <RootNavigator />
       </NavigationContainer>
     </TailwindProvider>
-  );
+  ); 
 }
--- navigator/TabNavigator.tsx
+++ navigator/TabNavigator.tsx
@@ -1,12 +1,26 @@
 import { View, Text } from 'react-native';
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useNavigation } from '@react-navigation/native';
 import CustomerScreen from '../screens/CustomerScreen';
 import OrdersScreen from '../screens/OrdersScreen';
 
-const Tab = createBottomTabNavigator();
+export type TabStackParamList = {
+  Customers: undefined;
+  Orders: undefined;
+};
+
+const Tab = createBottomTabNavigator<TabStackParamList>();
 
 const TabNavigator = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
     <Tab.Navigator>
       <Tab.Screen name='Customers' component={CustomerScreen} />
