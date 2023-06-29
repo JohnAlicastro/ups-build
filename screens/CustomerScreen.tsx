0edf7301115bd17850abc11b9237fa9ae9f0c2a9
John Alicastro <93687275+JohnAlicastro@users.noreply.github.com>
2023-06-28 20:43:21 -0400

    Setup react native navigation

--- screens/CustomerScreen.tsx
+++ screens/CustomerScreen.tsx
@@ -1,14 +1,14 @@
-import { View, Text } from 'react-native';
+import { View, Text, SafeAreaView } from 'react-native';
 import React from 'react';
 import { useTailwind } from 'tailwind-rn';
 
 const CustomerScreen = () => {
-  const tailwind = useTailwind();
+  const tw = useTailwind();
 
   return (
-    <View>
-      <Text>CustomerScreen</Text>
-    </View>
+    <SafeAreaView>
+      <Text style={tw('text-blue-500')}>CustomerScreen</Text>
+    </SafeAreaView>
   );
 };
 
