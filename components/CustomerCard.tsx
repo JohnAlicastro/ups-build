6f0d831fb70d217eaf2d2ccef955869ef4b67111
John Alicastro <93687275+JohnAlicastro@users.noreply.github.com>
2023-07-07 16:03:38 -0400

    Style CustomerCard component

--- components/CustomerCard.tsx
+++ components/CustomerCard.tsx
@@ -21,14 +21,16 @@ const CustomerCard = ({ email, name, userId }: CustomerCardProps) => {
     <TouchableOpacity>
       <Card containerStyle={tw('p-5 rounded-lg')}>
         <View>
-          <View>
-            <Text style={tw('')}>{name}</Text>
-            <Text>ID: {userId}</Text>
-          </View>
+          <View style={tw('flex-row justify-between')}>
+            <View>
+              <Text style={tw('text-2xl font-bold')}>{name}</Text>
+              <Text style={[tw('text-sm'), { color: '#59C1CC' }]}>ID: {userId}</Text>
+            </View>
 
-          <View>
-            <Text>{loading ? 'loading...' : `${orders.length} x`}</Text>
-            <Icon style={tw('mb-5 ml-auto')} name='box' type='entypo' color='#59C1CC' size={50} />
+            <View style={tw('flex-row items-center justify-end')}>
+              <Text style={{ color: '#59C1CC' }}>{loading ? 'loading...' : `${orders.length} x`}</Text>
+              <Icon style={tw('mb-5 ml-auto')} name='box' type='entypo' color='#59C1CC' size={50} />
+            </View>
           </View>
         </View>
         <Card.Divider />
