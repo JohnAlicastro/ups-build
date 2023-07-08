33da0a911101c28ef5d2f0bcf940afd0ad926792
John Alicastro <93687275+JohnAlicastro@users.noreply.github.com>
2023-07-08 14:26:32 -0400

    Style OrderCard component

--- components/OrderCard.tsx
+++ components/OrderCard.tsx
@@ -19,6 +19,13 @@ const OrderCard = ({ item }: OrderCardProps) => {
             <Text style={{ fontSize: 10 }}>{new Date(item.createdAt).toDateString()}</Text>
           </View>
 
+          <View>
+            <Text style={[tw('text-gray-400'), { fontSize: 10 }]}>
+              {item.carrier}-{item.trackingId}
+            </Text>
+            <Text style={tw('text-gray-500 text-xl')}>{item.trackingItems.customer.name}</Text>
+          </View>
+
           <View style={tw('flex-row items-center')}>
             <Text style={[tw('text-sm'), { color: '#EB6A7C' }]}>{item.trackingItems.items.length} x</Text>
             <Icon style={tw('ml-2')} name='box' type='feather'></Icon>
