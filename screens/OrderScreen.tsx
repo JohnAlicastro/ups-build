0dff452168fd52c947e72ff9aa82b9cf867d8e4a
John Alicastro <93687275+JohnAlicastro@users.noreply.github.com>
2023-07-08 14:55:15 -0400

    Style OrderScreen

--- screens/OrderScreen.tsx
+++ screens/OrderScreen.tsx
@@ -1,12 +1,37 @@
 import { View, Text } from 'react-native';
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
+import { useTailwind } from 'tailwind-rn/dist';
+import { CompositeNavigationProp, RouteProp, useNavigation, useRoute } from '@react-navigation/native';
+import { TabStackParamList } from '../navigator/TabNavigator';
+import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../navigator/RootNavigator';
+
+export type OrdersScreenNavigationProp = CompositeNavigationProp<
+  BottomTabNavigationProp<TabStackParamList, 'Orders'>,
+  NativeStackNavigationProp<RootStackParamList>
+>;
+
+type OrderScreenRouteProp = RouteProp<RootStackParamList, 'Order'>;
 
 const OrderScreen = () => {
-  return (
-    <View>
-      <Text>OrderScreen</Text>
-    </View>
-  );
+  const tw = useTailwind();
+  const navigation = useNavigation<OrdersScreenNavigationProp>();
+
+  const {
+    params: { order },
+  } = useRoute<OrderScreenRouteProp>();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerTitle: order.trackingItems.customer.name,
+      headerTitleStyle: { color: 'black' },
+      headerBackTitle: 'Deliveries',
+      headerTintColor: '#EB6A7C',
+    });
+  }, [order]);
+
+  return <View style={tw('-mt-2')}></View>;
 };
 
 export default OrderScreen;
